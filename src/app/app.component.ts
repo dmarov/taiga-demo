cab156453909b0f4d12eca8c8ab464392b2564e3
Dmitry Marov <d.marov94@gmail.com>
2024-04-08 18:38:33 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER, TuiTextfieldControllerModule } from "@taiga-ui/core";
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { TuiInputModule } from "@taiga-ui/kit";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TuiRootModule, TuiDialogModule, TuiAlertModule, ReactiveFormsModule, TuiInputModule, TuiTextfieldControllerModule ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+  styleUrl: './app.component.less',
+  providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}]
 })
 export class AppComponent {
-  title = 'taiga-demo';
+  readonly testForm = new FormGroup({
+    testValue: new FormControl('mail@mail.ru'),
+  });
 }
+
